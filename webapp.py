0e2fdccd2566efd90f21f9c228233804f521d1dc
JeremyKnight <jeremyknightjck99@gmail.com>
2017-10-10 09:28:47 -0700

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,24 @@
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+@app.route("/response")
+def render_response():
+    length = request.args['length']
+    
+    #The request object stores information about the request sent to the server.
+    #args is a MultiDict (like a dictionary but can have multiple values for the same key)
+    #The information in args is visible in the url for the page being requested (ex. .../response?color=blue)
+    
+    if float(color) == 'pink':
+        response = "That's my favorite color, too!"
+    else:
+        response = "My favorite color is pink."
+    return render_template('response.html', response = response)
+    
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
